dbb487b92fc361bef0677ee96c768a31d5f97510
ruoyu <412244196@qq.com>
2023-03-03 11:59:27 +0800

    配置

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,24 @@
 export default {
-    title: 'ESP32快速学习',
-    description: '玩转ESP32',
-    base: '/esp32-starter/'
+  title: 'ESP32快速学习',
+  description: '玩转ESP32',
+  base: '/esp32-starter/',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Python速学',
+        items: [
+          { text: '基础语法', link: '/python' }
+        ]
+      },
+      {
+        text: '实验',
+        items: [
+          { text: '点亮LED', link: '/点亮LED' },
+          { text: '开关中断', link: '/开关中断' },
+        ]
+      }
+    ],
+
+
+  }
 }
